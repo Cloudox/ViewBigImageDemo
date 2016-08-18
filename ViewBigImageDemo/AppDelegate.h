cad5abc6bd9f1e6004890cacde0f931913d35960
Cloudox <cloudox@foxmail.com>
2016-08-18 19:25:16 +0800

    Initial Commit

---
+++ ViewBigImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewBigImageDemo
+//
+//  Created by Cloudox on 16/8/18.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
